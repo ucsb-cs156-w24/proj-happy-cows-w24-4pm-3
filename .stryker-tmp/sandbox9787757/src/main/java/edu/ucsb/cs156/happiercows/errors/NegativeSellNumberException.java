5104a9fedb52410023b1077124ab8ed7a401775b
bonniehuynh <bonniehuynh@ucsb.edu>
2024-02-29 17:39:03 -0800

    bh - deleted stryker tmp file

--- .stryker-tmp/sandbox9787757/src/main/java/edu/ucsb/cs156/happiercows/errors/NegativeSellNumberException.java
+++
@@ -1,7 +0,0 @@
-package edu.ucsb.cs156.happiercows.errors;
-
-public class NegativeSellNumberException extends Exception {
-    public NegativeSellNumberException(String messageString){
-        super(messageString);
-    }
-}
